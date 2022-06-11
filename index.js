da62ed049198f0f7183e73dce3338388e50a1f4e
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-11 12:36:44 +0300

    commit tree de7c4167589be67fc073c2472b7a79054c3228b1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-const totalBatteries = batteryBatches.reduce((accumulator,element)=>element + accumulator,0)
+// const totalBatteries = batteryBatches.reduce((accumulator,element)=>element + accumulator,0)
 // console.log(totalBatteries)
+
+const totalBatteries = batteryBatches.reduce((accumulator,element)=> element + accumulator,0)
